e1b26d70c08c658c26e6e466eb386ef3db630bb1
Беляев Максим Дмитриевич <mdbelyaev@edu.tversu.net>
2020-02-26 10:48:53 +0300

    Добавьте файлы проекта.

---
+++ Stroki/Source.cpp
@@ -0,0 +1,10 @@
+// Stroki.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Stroki.h"
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
