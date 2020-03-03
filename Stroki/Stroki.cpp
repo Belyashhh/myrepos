dffe13826bfb1ebb6aa67fcc81f9e2475fa602f7
Макс Беляев <maks.belyaev01@yandex.ru>
2020-03-03 12:34:38 +0300

    str1

--- Stroki/Stroki.cpp
+++ Stroki/Stroki.cpp
@@ -1,5 +1,28 @@
 ﻿#include <iostream>
 #include "Stroki.h"
 
+Stroki::Stroki()
+{
+}
+
+Stroki::Stroki(char* s)
+{
+	n = strlen(s);
+	str = new char[n+1];
+	strcpy_s(str, n + 1, s);
+}
 
 
+Stroki::Stroki(const char* s)
+{
+	n = strlen(s);
+	str = new char[n + 1];
+	strcpy_s(str, n + 1, s);
+}
+
+Stroki::Stroki(const Stroki& a)
+{
+	n = a.n;
+	str = new char[n + 1];
+	strcpy_s(str, n + 1, a.str);
+}
--- Stroki/Stroki.h
+++ Stroki/Stroki.h
@@ -1,6 +1,13 @@
 #pragma once
 class Stroki
 {
+	char* str;
+	int n;
+public:
+	Stroki();
+	Stroki(char* s);
+	Stroki(const char* s);
+	Stroki(const Stroki& a);
 
 };
 
