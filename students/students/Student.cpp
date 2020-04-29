77cf6ccc12509f6e64fc043bc2a7253a3798ebb1
Макс Беляев <maks.belyaev01@yandex.ru>
2020-04-29 14:17:31 +0300

    (◉◡◉)

--- students/students/Student.cpp
+++ students/students/Student.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Student.h"
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -8,6 +9,14 @@ void Student::copy(int* a)
 {
 	for (int i = 0; i < 5; i++)
 		ball[i] = a[i];
+}
+
+float Student::averagemark(int* a)
+{
+	float n = 0;
+	for (int i = 0; i < 5; i++)
+		n =+ a[i];
+	return n/5;
 }
 
 Student::Student()
@@ -46,10 +55,68 @@ Student::Student(string s)
 		name += s[i];
 }
 
+Student::Student(Student& s)
+{
+	name = s.name;
+	gr = s.gr;
+	copy(s.ball);
+}
+
+Student& Student::operator=(Student& s)
+{
+	this->name = s.name;
+	this->gr = s.gr;
+	this->copy(s.ball);
+	return *this;
+}
+
 void Student::print()
 {
 	cout << name << ' ' << gr << ' ';
 	for (int i = 0; i < 5; i++)
 		cout << ball[i];
 	cout << endl;
+}
+
+bool Student::operator>(Student& a)
+{
+	bool n = true;
+	if (gr < a.gr)
+		n = false;
+	if (gr == a.gr)
+		if (name <= a.name)
+			n = (averagemark(ball) > averagemark(a.ball));
+	return n;
+}
+
+bool Student::operator<(Student& a)
+{
+	bool n = true;
+	if (gr > a.gr)
+		n = false;
+	if (gr == a.gr)
+		if (name >= a.name)
+			n = (averagemark(ball) < averagemark(a.ball));
+	return n;
+}
+
+bool Student::operator==(Student& a)
+{
+	return gr == a.gr && name==a.name && averagemark(ball) == averagemark(a.ball);
+}
+
+bool Student::operator!=(Student& a)
+{
+	return gr != a.gr || name != a.name || averagemark(ball) != averagemark(a.ball);
+}
+
+ostream& operator<<(ostream& os, const Student& a)
+{
+	return os << a.name << " " << a.gr ;
+
+}
+
+istream& operator>>(istream& os, Student& a)
+{
+	return os >> a.name >> a.gr;
 }
--- students/students/Student.h
+++ students/students/Student.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <iostream>
+
 using namespace std;
 
 class Student
@@ -9,11 +10,22 @@ class Student
 	int gr;
 	int ball[5] = { 0 };
 	void copy(int* a);
-	
+	float averagemark(int* a);
+
 public:
 	Student();
 	Student(string s, int g, int* a);
 	Student(string s);
+	Student(Student& s);
+	Student& operator=(Student& s);
 	void print();
+
+	bool operator>(Student& a);
+	bool operator<(Student& a);
+	bool operator==(Student& a);
+	bool operator!=(Student& a);
+
+	friend ostream& operator << (ostream& os, const Student& a);
+	friend istream& operator >>(istream& os, Student& a);
 };
 
